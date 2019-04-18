a76733d102f8021c839380a000e0aaa1749e29f4
Mihir Atmakuri <mihir.atmakuri@gmail.com>
2019-04-17 22:38:53 -0500

    Update TwoSum.java
    - added documentation

--- TwoSum.java
+++ TwoSum.java
@@ -1,14 +1,31 @@
+/**
+ * This is a Two Sum class. This class accepts a numbers array and a target number.
+ * This class calculates if the target number can be formed by adding 2 numbers present in the array.
+ */
 public TwoSum {
-
+/**
+ * Integer array to hold the numbers.
+ */
 private int[] mNums;
-
+/**
+ * This is an integer that holds the target number.
+ */
 private int mTarget;
 
+/**
+ * Constructor
+ * @param nums number array
+ * @param trgt the target number
+ */
 public TwoSum(int[] nums, int trgt) {
 	mNums = nums;
 	mTarget = trgt;
 }
 
+/**
+ * This method returns if the array contains 2 numbers that add up to the given target
+ * returns a boolean True 
+ */
 public boolean result() {
 	try {
 		int[] nums = twoSum();
@@ -19,6 +36,10 @@ public boolean result() {
 	return false;
 }
 
+/**
+ * This method returns an integer array with the indices of the input that add up to the target.
+ * If no solution exists, this method throws {IllegalArgumentException} exception.
+ */
 public int[] twoSum() {
         HashMap<Integer, Integer> map = new HashMap<>();
         int[] solution = new int[2];
